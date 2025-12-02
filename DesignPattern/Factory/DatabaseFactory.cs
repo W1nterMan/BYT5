64b9a3b331cc411d01599e441d5083dfaeac53f6
WinterMan <vityrogoza@gmail.com>
2025-12-02 11:35:40 +0100

    Factory implemented

---
+++ DesignPattern/Factory/DatabaseFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPattern.Factory
+{
+    public static class DatabaseFactory
+    {
+        public static IDatabase CreateDatabase(DatabaseType sqlServer)
+        {
+            if (sqlServer == DatabaseType.SqlServer)
+            {
+                return new SqlServerDatabase();
+            } else if (sqlServer == DatabaseType.Oracle)
+            {
+                return new OracleDatabase();
+            }
+
+            return null;
+        }
+    }
+}
